0cdb0c57c6bc9dd4186b57edc4b36478aa17626b
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-05-30 20:56:23 +0300

    Задание 35.5.2

---
+++ SocialNet.Data/Repositories/MessagesRepository.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialNet.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNet.Data.Repositories
+{
+    public class MessagesRepository : Repository<Message>
+    {
+        public MessagesRepository(ApplicationDbContext db) : base(db)
+        {
+
+        }
+
+        public List<Message> GetMessages(User sender, User recipient)
+        {
+            Set.Include(x => x.Recipient);
+            Set.Include(x => x.Sender);
+
+            var from = Set.AsEnumerable().Where(x => x.SenderId == sender.Id && x.RecipientId == recipient.Id).ToList();
+            var to = Set.AsEnumerable().Where(x => x.SenderId == recipient.Id && x.RecipientId == sender.Id).ToList();
+
+            var itog = new List<Message>();
+            itog.AddRange(from);
+            itog.AddRange(to);
+            itog.OrderBy(x => x.Id);
+            return itog;
+        }
+    }
+}
