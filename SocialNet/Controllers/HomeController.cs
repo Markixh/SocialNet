d6d0559ff5683af2e1f570d2b854ce153833de0a
Марков Андрей Анатольевич <andrejmarko@yandex.ru>
2023-06-01 01:50:53 +0300

    Задание 35.6.5

--- SocialNet/Controllers/HomeController.cs
+++ SocialNet/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SocialNet.Data.Models;
 using SocialNet.ViewModels.Account;
 using System.Diagnostics;
 
@@ -7,18 +9,28 @@ namespace SocialNet.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<User> _signInManager;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, SignInManager<User> signInManager)
         {
             _logger = logger;
+            _signInManager = signInManager;
         }
 
+        [Route("")]
+        [Route("[controller]/[action]")]
         public IActionResult Index()
         {
-            var mainModel = new MainViewModel();
-
-            return View(mainModel);
+            if (_signInManager.IsSignedIn(User))
+            {
+                return RedirectToAction("MyPage", "AccountManager");
+            }
+            else
+            {
+                return View(new MainViewModel());
+            }
         }
+
 
         public IActionResult Privacy()
         {
@@ -30,5 +42,7 @@ namespace SocialNet.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+
     }
 }
